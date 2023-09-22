d6596943df30c0fa83da18001655312fde9e5778
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-09-22 16:18:21 +0900

    first commit

---
+++ Original-CameraView/Original_CameraViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Original_CameraViewApp.swift
+//  Original-CameraView
+//
+//  Created by いっちゃん on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Original_CameraViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
